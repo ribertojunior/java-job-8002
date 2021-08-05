401652d37419fd039d04b811cdc4aa3eb0f33d6f
Riberto Junior <ribertojunior@gmail.com>
2021-08-05 17:02:12 -0300

    Initial commit

---
+++ src/main/java/br/com/trigg/javajob8002/JavaJob8002Application.java
@@ -0,0 +1,13 @@
+package br.com.trigg.javajob8002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJob8002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJob8002Application.class, args);
+	}
+
+}
