d336435b5c8fe6257dd45a162b1c7a2836c5ac6b
Riberto Junior <ribertojunior@gmail.com>
2021-08-08 10:09:00 -0300

    Bouncing Ball

---
+++ codewars/B.java
@@ -0,0 +1,7 @@
+public class B {
+  public static int bouncingBall(double h, double bounce, double window) {
+    if (h <= 0 || (bounce <= 0 || bounce >= 1) || window >= h) return -1;
+    double count = Math.log(window/h) /Math.log(bounce);
+    return (int) (Math.floor(count) * 2) + 1;
+  }
+}
