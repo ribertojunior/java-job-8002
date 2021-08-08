595242e30f501f1887d42f787c72ad762d80d433
Riberto Junior <ribertojunior@gmail.com>
2021-08-08 10:27:18 -0300

    Correção bouncingBall

--- codewars/B.java
+++ codewars/B.java
@@ -2,6 +2,6 @@ public class B {
   public static int bouncingBall(double h, double bounce, double window) {
     if (h <= 0 || (bounce <= 0 || bounce >= 1) || window >= h) return -1;
     double count = Math.log(window/h) /Math.log(bounce);
-    return (int) (Math.floor(count) * 2) + 1;
+    return count == 1 ? 1 : (int) (Math.floor(count) * 2) + 1;
   }
 }
